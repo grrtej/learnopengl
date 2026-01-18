a77ae1a2d00eff97729c8b0193be136dd3444b13
Gurtej Saini <grrtej@gmail.com>
2026-01-18 15:13:49 -0600

    create window

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,59 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
+void error_callback(int error, const char* description);
+void framebuffer_size_callback(GLFWwindow* window, int w, int h);
+void process_input(GLFWwindow* window);
+
 int main()
 {
-    std::println("you just won the internet my good sir");
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "learnopengl", nullptr, nullptr);
+	if (!window) {
+		std::println(stderr, "failed to create glfw window");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// glad init
+	gladLoadGL(glfwGetProcAddress);
+
+	glfwSetErrorCallback(error_callback);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	glViewport(0, 0, 800, 600);
+
+	while (!glfwWindowShouldClose(window)) {
+		process_input(window);
+
+		glClearColor(.2f, .3f, .3f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+}
+
+void error_callback(int error, const char* description)
+{
+	std::println(stderr, "glfw error: {}\n", description);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+void process_input(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
